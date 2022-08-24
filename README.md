4a1faa8e66a9881ac2db53bcdc4719bc74fb6ad3
Sergey Kovalevich <inndie@gmail.com>
2022-08-24 20:55:34 +0300

    update

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Simple Binary Encoding codec generator for C++20 [WIP]
 
 * spec https://github.com/FIXTradingCommunity/fix-simple-binary-encoding
 * inspired by https://github.com/real-logic/simple-binary-encoding
+
+## Sample usage
+
+```sh
+$ python3 -m app --schema=resources/b3-market-data-messages-1.3.1.xml --destination=$PWD/result
+```
--- app/__main__.py
+++ app/__main__.py
@@ -1,12 +1,17 @@
+from argparse import ArgumentParser
 from app.schema import Schema
 from app.generator_cpp import GeneratorCpp
 
 PATH = 'b3-market-data-messages-1.3.1.xml'
 
 def main() -> None:
-    # TODO: args
-    schema = Schema.loadFromFile(PATH)
-    GeneratorCpp('result').run(schema)
+    parser = ArgumentParser(prog='sbe-code-gen', description='SBE codec generator')
+    parser.add_argument('--schema', help='path to xml schema', required=True)
+    parser.add_argument('--destination', help='path to directory where codec will be written', required=True)
+    args = parser.parse_args()
+
+    schema = Schema.loadFromFile(args.schema)
+    GeneratorCpp(args.destination).run(schema)
 
 if __name__ == '__main__':
     main()
