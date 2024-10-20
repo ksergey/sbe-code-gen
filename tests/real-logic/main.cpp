dce9769f98e78ca25f2d517faa2e29dfdcba80f7
Sergey Kovalevich <inndie@gmail.com>
2024-10-20 12:25:05 +0300

    update

--- tests/real-logic/main.cpp
+++ tests/real-logic/main.cpp
@@ -1,17 +1,43 @@
 // Copyright (c) Sergey Kovalevich <inndie@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0
 
-#include <fmt/format.h>
+#include <array>
+
+#include <doctest/doctest.h>
 
 #include "baseline/Car.h"
 #include "baseline/MessageHeader.h"
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
-  try {
+using namespace baseline;
 
-  } catch (std::exception const& e) {
-    fmt::print(stderr, "ERROR: {}\n", e.what());
-    return EXIT_FAILURE;
+std::size_t encodeMessageHeader(MessageHeader& header) noexcept {
+  header.field<"blockLength">().setValue(Car::sbeBlockLength());
+  header.field<"templateId">().setValue(Car::sbeTemplateId());
+  header.field<"schemaId">().setValue(Car::sbeSchemaId());
+  header.field<"version">().setValue(Car::sbeSchemaVersion());
+  return header.encodedLength();
+}
+
+TEST_CASE("MessageHeader") {
+  std::array<std::byte, 4096> storage;
+
+  {
+    MessageHeader header(storage.data(), storage.size());
+
+    CHECK(encodeMessageHeader(header) == 8);
+    CHECK(header.field<"blockLength">().value() == Car::sbeBlockLength());
+    CHECK(header.field<"templateId">().value() == Car::sbeTemplateId());
+    CHECK(header.field<"schemaId">().value() == Car::sbeSchemaId());
+    CHECK(header.field<"version">().value() == Car::sbeSchemaVersion());
   }
-  return EXIT_SUCCESS;
+
+  {
+    MessageHeader header(storage.data(), 0, storage.size(), Car::sbeSchemaVersion());
+
+    CHECK(encodeMessageHeader(header) == 8);
+    CHECK(header.field<"blockLength">().value() == Car::sbeBlockLength());
+    CHECK(header.field<"templateId">().value() == Car::sbeTemplateId());
+    CHECK(header.field<"schemaId">().value() == Car::sbeSchemaId());
+    CHECK(header.field<"version">().value() == Car::sbeSchemaVersion());
+  }
 }
