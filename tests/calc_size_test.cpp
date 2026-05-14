343184ff2fff7365d73549501896bff029814643
Sergey Kovalevich <inndie@gmail.com>
2026-05-15 00:37:17 +0300

    refactoring

--- tests/calc_size_test.cpp
+++ tests/calc_size_test.cpp
@@ -5,14 +5,14 @@
 #include "schema.h"
 
 TEST_CASE("calc_size: simple") {
-    REQUIRE_EQ(spot_sbe::NonRepresentableMessage::calcBufferSize(), 0);
-    REQUIRE_EQ(spot_sbe::PriceFilter::calcBufferSize(), 1 + 8 + 8 + 8);
-    REQUIRE_EQ(spot_sbe::IcebergPartsFilter::calcBufferSize(), 8);
-    REQUIRE_EQ(spot_sbe::PingResponse::calcBufferSize(), 0);
+    REQUIRE_EQ(spot_sbe::NonRepresentableMessage::sbeComputeSize(), 0);
+    REQUIRE_EQ(spot_sbe::PriceFilter::sbeComputeSize(), 1 + 8 + 8 + 8);
+    REQUIRE_EQ(spot_sbe::IcebergPartsFilter::sbeComputeSize(), 8);
+    REQUIRE_EQ(spot_sbe::PingResponse::sbeComputeSize(), 0);
 }
 
 TEST_CASE("calc_size: repeating groups") {
-    REQUIRE_EQ(spot_sbe::WebSocketSessionSubscriptionsResponse::calcBufferSize({4}), (6 + 4 * (2 + 8)));
-    REQUIRE_EQ(spot_sbe::TradesResponse::calcBufferSize({1}), 1 + 1 + (6 + 1 * (8 + 8 + 8 + 8 + 8 + 1 + 1)));
-    REQUIRE_EQ(spot_sbe::TradesResponse::calcBufferSize({3}), 1 + 1 + (6 + 3 * (8 + 8 + 8 + 8 + 8 + 1 + 1)));
+    REQUIRE_EQ(spot_sbe::WebSocketSessionSubscriptionsResponse::sbeComputeSize({4}), (6 + 4 * (2 + 8)));
+    REQUIRE_EQ(spot_sbe::TradesResponse::sbeComputeSize({1}), 1 + 1 + (6 + 1 * (8 + 8 + 8 + 8 + 8 + 1 + 1)));
+    REQUIRE_EQ(spot_sbe::TradesResponse::sbeComputeSize({3}), 1 + 1 + (6 + 3 * (8 + 8 + 8 + 8 + 8 + 1 + 1)));
 }
